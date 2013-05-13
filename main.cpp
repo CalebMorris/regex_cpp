08eeb0ed0fb0ae55572dc0c3134bddf5578ea04b
Caleb Morris <caleb.morris.g@gmail.com>
2013-05-13 16:05:41 -0700

    BUG fixed

--- main.cpp
+++ main.cpp
@@ -3,16 +3,7 @@
 #include "regex.hpp"
 
 int main () {
-	//BUG: When multiple instances if Regex exist, there
-	// are intermitent segfaults or wrong answers
 	Regex r("e{1,}");
-	Regex s("e{1}");
-	Regex t("e{1,2}");
 	std::cout << r.match("e") << std::endl;
-	std::cout << r.match("ete") << std::endl;
-	std::cout << s.match("e") << std::endl;
-	std::cout << s.match("ete") << std::endl;
-	std::cout << t.match("e") << std::endl;
-	std::cout << t.match("ete") << std::endl;
 	return 0;
 }
