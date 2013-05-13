eea804ef9abea6e5a0eb018e9489c59a13ed4669
Caleb Morris <caleb.morris.g@gmail.com>
2013-05-13 14:10:15 -0700

    simple single runs to figure out bugs

--- main.cpp
+++ main.cpp
@@ -3,28 +3,8 @@
 #include "regex.hpp"
 
 int main () {
-	Regex r("t?");
-	/*
-	std::cout << r.regex_to_postfix("t") << std::endl;
-	std::cout << r.regex_to_postfix("te") << std::endl;
-	std::cout << r.regex_to_postfix("t|e") << std::endl;
-	std::cout << r.regex_to_postfix("tes|e") << std::endl;
-	std::cout << r.regex_to_postfix("t|est") << std::endl;
-	std::cout << r.regex_to_postfix("test|er") << std::endl;
-	std::cout << r.regex_to_postfix("te|st|er") << std::endl;
-	std::cout << r.regex_to_postfix("t*") << std::endl;
-	std::cout << r.regex_to_postfix("t+e") << std::endl;
-	std::cout << r.regex_to_postfix("t*es") << std::endl;
-	std::cout << r.regex_to_postfix("te?s") << std::endl;
-	std::cout << r.regex_to_postfix("(t)") << std::endl;
-	std::cout << r.regex_to_postfix("(t*)") << std::endl;
-	std::cout << r.regex_to_postfix("(t)*") << std::endl;
-	std::cout << r.regex_to_postfix("(t*)*") << std::endl;
-	std::cout << r.regex_to_postfix("(te)*") << std::endl;
-	std::cout << r.regex_to_postfix("ab(te)st") << std::endl;
-	std::cout << r.regex_to_postfix("(a|b)s") << std::endl;
-	*/
-	std::cout << r.toString() << std::endl;
-	std::cout << r.match("t") << std::endl;
+	Regex r("e{1,}");
+	std::cout << r.match("e") << std::endl;
+	std::cout << r.match("ete") << std::endl;
 	return 0;
 }
